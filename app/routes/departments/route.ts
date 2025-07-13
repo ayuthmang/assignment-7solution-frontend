63d23b5c3fd2f3e51a270996590563616d6f6570
Ayuth Mangmesap <ayuth.mang@gmail.com>
2025-07-13 21:11:50 +0700

    feat: department api

--- app/routes/departments/route.ts
+++ app/routes/departments/route.ts
@@ -1,26 +1,128 @@
-import {data} from "@remix-run/node";
-import {UserResponse} from "./type";
-import {groupBy} from 'lodash'
+import { data } from "@remix-run/node";
+import { UserResponse, type User } from "./type";
 
 async function getUsers(): Promise<UserResponse> {
-    const response = await fetch("https://dummyjson.com/users");
-    return await response.json();
+  const response = await fetch("https://dummyjson.com/users");
+  return await response.json();
 }
 
-function usersByDepartment(users: UserResponse) {
-    return groupBy(users.users, user => user.company.department || "Unknown");
-
-    // return users.users.reduce((acc, user) => {
-    //     const department = user.company.department || "Unknown";
-    //     if (!acc[department]) {
-    //         acc[department] = [];
-    //     }
-    //     acc[department].push(user);
-    //     return acc;
-    // }, {} as Record<string, UserResponse["users"]>);
+export function usersByDepartment(users: User[]) {
+  return users.reduce((acc, user) => {
+    const department = user.company.department || "Unknown";
+    if (!acc[department]) {
+      acc[department] = [];
+    }
+    acc[department].push(user);
+    return acc;
+  }, {} as Record<string, UserResponse["users"]>);
 }
 
+/**
+ * {
+        [Department]: {
+            "male": 1,                      // ---> Male Count Summary
+            "female": 1,                    // ---> Female Count Summary
+            "ageRange": "XX-XX",            // ---> Range
+            "hair": {                       // ---> "Color": Color Summary
+                "Black": 1,
+                "Blond": 1,
+                "Chestnut": 1,
+                "Brown": 1
+            },
+            "addressUser": {                // ---> "firstNamelastName": postalCode
+                "TerryMedhurst": "XXXXX",
+            }
+        }
+    },
+ */
+export function summarizeUsers(users: User[]) {
+  const summaryByDepartment = users.reduce(
+    (acc, curr) => {
+      const maleCount = curr.gender === "male" ? 1 : 0;
+      const femaleCount = curr.gender === "female" ? 1 : 0;
+
+      acc["male"] += maleCount;
+      acc["female"] += femaleCount;
+
+      // Age Range
+      const age = curr.age;
+      acc["_ageRange"].push(age);
+
+      // Hair Color
+      const hairColor = curr.hair.color;
+      if (hairColor) {
+        acc["hair"][hairColor] = (acc["hair"][hairColor] || 0) + 1;
+      }
+
+      // Address User
+      const addressKey = `${curr.firstName}${curr.lastName}`;
+      acc["addressUser"][addressKey] = curr.address.postalCode;
+
+      return acc;
+    },
+    {
+      male: 0,
+      female: 0,
+      ageRange: "", // 'xx-xx'
+      _ageRange: [], // to collect all age ranges
+      hair: {
+        // [color: string]: number;
+      },
+      addressUser: {
+        // [key: string]: string; // firstName + lastName as key, postalCode as value
+      },
+    } as unknown as {
+      male: number;
+      female: number;
+      ageRange: string; // 'xx-xx'
+      _ageRange: number[]; // to collect all age ranges
+      hair: {
+        [color: string]: number;
+      };
+      addressUser: {
+        [key: string]: string; // firstName + lastName as key, postalCode as value
+      };
+    }
+  );
+
+  const minAge = Math.min(...summaryByDepartment._ageRange);
+  const maxAge = Math.max(...summaryByDepartment._ageRange);
+  summaryByDepartment.ageRange = `${minAge}-${maxAge}`;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error // I cannot make typescript happy here
+  delete summaryByDepartment._ageRange;
+
+  return summaryByDepartment;
+}
+
+/**
+ * @returns  {
+        [Department]: {
+            "male": 1,                      // ---> Male Count Summary
+            "female": 1,                    // ---> Female Count Summary
+            "ageRange": "XX-XX",            // ---> Range
+            "hair": {                       // ---> "Color": Color Summary
+                "Black": 1,
+                "Blond": 1,
+                "Chestnut": 1,
+                "Brown": 1
+            },
+            "addressUser": {                // ---> "firstNamelastName": postalCode
+                "TerryMedhurst": "XXXXX",
+            }
+        }
+    },
+ */
 export async function loader() {
-    const users = await getUsers()
-    return data(usersByDepartment(users));
+  const { users } = await getUsers();
+  const _usersByDepartment = usersByDepartment(users);
+  const summarizedData = Object.entries(_usersByDepartment).reduce(
+    (acc, [department, users]) => {
+      acc[department] = summarizeUsers(users);
+      return acc;
+    },
+    {} as Record<string, ReturnType<typeof summarizeUsers>>
+  );
+  console.log({ summarizedData });
+  return data(summarizedData);
 }
--- app/routes/departments/type.ts
+++ app/routes/departments/type.ts
@@ -1,69 +1,76 @@
 export interface UserResponse {
-    users: User[];
-    total: number;
-    skip: number;
-    limit: number;
+  users: User[];
+  total: number;
+  skip: number;
+  limit: number;
 }
 
 export interface User {
-    id: number;
-    firstName: string;
-    lastName: string;
-    maidenName: string;
-    age: number;
-    gender: string;
-    email: string;
-    phone: string;
-    username: string;
-    password: string;
-    birthDate: string;
-    image: string;
-    bloodGroup: string;
-    height: number;
-    weight: number;
-    eyeColor: string;
-    hair: Hair;
-    domain: string;
-    ip: string;
-    address: Address;
-    macAddress: string;
-    university: string;
-    bank: Bank;
-    company: Company;
-    ein: string;
-    ssn: string;
-    userAgent: string;
+  id: number;
+  firstName: string;
+  lastName: string;
+  maidenName: string;
+  age: number;
+  gender: string;
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  birthDate: string;
+  image: string;
+  bloodGroup: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+  hair: Hair;
+  ip: string;
+  address: Address;
+  macAddress: string;
+  university: string;
+  bank: Bank;
+  company: Company;
+  ein: string;
+  ssn: string;
+  userAgent: string;
+  crypto: {
+    coin: string;
+    wallet: string;
+    network: string;
+  };
+  role: string;
 }
 
 export interface Hair {
-    color: string;
-    type: string;
+  color: string;
+  type: string;
 }
 
 export interface Address {
-    address: string;
-    city: string;
-    coordinates: Coordinates;
-    postalCode: string;
-    state: string;
+  address: string;
+  city: string;
+  coordinates: Coordinates;
+  postalCode: string;
+  state: string;
+  stateCode: string;
+  country: string;
 }
 
 export interface Coordinates {
-    lat: number;
-    lng: number;
+  lat: number;
+  lng: number;
 }
 
 export interface Bank {
-    cardExpire: string;
-    cardNumber: string;
-    cardType: string;
-    currency: string;
-    iban: string;
+  cardExpire: string;
+  cardNumber: string;
+  cardType: string;
+  currency: string;
+  iban: string;
 }
 
 export interface Company {
-    address: Address;
-    department: string;
-    name: string;
-    title: string;
+  address: Address;
+  department: string;
+  name: string;
+  title: string;
 }
